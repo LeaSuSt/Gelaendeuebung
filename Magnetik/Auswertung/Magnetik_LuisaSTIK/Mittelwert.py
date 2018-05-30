26e17c3081e5ba0ec42fc1b7013d24768ca503f0
unetn <lea@linux-vuof.suse>
2018-05-30 09:08:41 +0200

    Auswertung Magnetik angefangen

--- Magnetik/Auswertung/Magnetik_LuisaSTIK/Mittelwert.py
+++ Magnetik/Auswertung/Magnetik_LuisaSTIK/Mittelwert.py
@@ -7,9 +7,20 @@ Created on Tue May 22 19:11:57 2018
 
 import numpy as np
 
-daten=np.genfromtxt('m21_m2.dat')
+#daten=np.genfromtxt('M22_M23_Nr2.txt')
 
-Feldstaerken = daten[:, 1:3]
+#Feldstaerken = daten[:, 1:3]
 
-Mittelwerte = np.mean(Feldstaerken, axis=1)
-print(Mittelwerte)
+#Mittelwerte = np.mean(Feldstaerken, axis=1)
+#print(Mittelwerte)
+
+#np.savetxt('M22_M23_Nr2_mean.txt', daten[1,:], Mittelwerte)
+
+#Ziel: <x> <Leerzeichen> <mw> in eine txt-Datei schreiben!!!
+
+x,y1,y2,y3=np.loadtxt('M22_M23_Nr2.txt', unpack=True)
+mw=(y1+y2+y3)/3.
+daten=np.array([len(x),2])
+daten[:,0]=x
+daten[:,1]=mw
+np.savetxt('M22_M23_Nr2_mean.txt', daten)
---
+++ Magnetik/Auswertung/kalibrierung.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+I, alpha=np.loadtxt('kalibrierung.dat', unpack=True)
+B=I*26.5
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(B, alpha)
+
+#To get coefficient of determination (r_squared)
+print("r-squared:", r_value**2)
+print(slope)
+print(1./slope)
+
+#Plot the data along with the fitted line
+#Noch umrechnen in nT!!!!
+plt.plot(B, alpha, 'o', label='Messwerte')
+plt.plot(B, intercept + slope*B, 'r', label='lineare Regression')
+plt.xlabel(r'$B_z$ in nT')
+plt.ylabel(r'Ablesewert in Skt')
+plt.legend()
+#plt.show()
+plt.savefig('kalibrierung.pdf')
