c4c5134a827527c8e355be51618cf2e605152c14
unetn <lea@linux-vuof.suse>
2018-06-04 21:05:47 +0200

    Magnet Fehler angefangen

---
+++ Magnetik/Auswertung/Traktor/traktor.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+y=np.loadtxt('traktortest7.dat', unpack=True)
+
+print(len(y))
+x=np.arange(0,30,1./8.)
+print(len(x))
+
+#plt.plot(x,y)
+plt.xlabel('Zeit in s')
+plt.ylabel('Gradient der Vertikalkomponente in nT/m')
+plt.grid()
+
+#plt.savefig('plot_traktor.pdf')
+
+n=97 #Anzahl geplotteter Messpunkte
+
+v=np.zeros(n)
+w=np.zeros(n)
+for i in range(0,n):
+	v[i]=x[i]
+	w[i]=y[i]
+
+plt.plot(v,w)
+plt.savefig('traktor_ausschnitt.pdf')
