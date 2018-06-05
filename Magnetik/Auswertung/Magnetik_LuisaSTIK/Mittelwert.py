f9f15f911bb5e8442693d91cd1e7e40cffb60dea
unetn <unetn@student.kit.edu>
2018-06-05 16:50:04 +0200

    Auswertung weiter

--- Magnetik/Auswertung/Magnetik_LuisaSTIK/Mittelwert.py
+++ Magnetik/Auswertung/Magnetik_LuisaSTIK/Mittelwert.py
@@ -6,6 +6,7 @@ Created on Tue May 22 19:11:57 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 #daten=np.genfromtxt('M22_M23_Nr2.txt')
 
@@ -20,6 +21,14 @@ import numpy as np
 
 x,y1,y2,y3=np.loadtxt('M22_M23_Nr2.txt', unpack=True)
 mw=(y1+y2+y3)/3.
+
+X,Y1,Y2=np.loadtxt('M28_M29_Neu.txt', unpack=True)
+MW=(Y1+Y2)/2.
+
+plt.plot(x, mw)
+plt.plot(X,MW)
+plt.show()
+
 daten=np.array([len(x),2])
 daten[:,0]=x
 daten[:,1]=mw
