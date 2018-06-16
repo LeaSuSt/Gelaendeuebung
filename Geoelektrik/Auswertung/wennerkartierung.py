16fda70c4e3941f851f3babc37b88c30aec82ced
Login uweni <uweni@fphct17.physik.uni-karlsruhe.de>
2018-06-16 21:03:33 +0200

    Geoelektrik

--- Geoelektrik/Auswertung/wennerkartierung.py
+++ Geoelektrik/Auswertung/wennerkartierung.py
@@ -10,8 +10,10 @@ import matplotlib.pyplot as plt
 
 #import
 data = np.genfromtxt('wennerkartierung.txt')
+x = data[:,0]
+x_reversed= x[::-1]-(16-7.5)
 
-plt.plot(data[:,0], data[:,1], 'o')
+plt.plot(x_reversed, data[:,1], 'o')
 
 plt.xlabel('Abstand in m')
 plt.ylabel('spezifischer elektrischer Widerstand in Ohm m')
